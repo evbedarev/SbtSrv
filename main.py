52facffc91550b3fe27a9e2ea21bbac3a36fb37f
madjo <madjo85@yandex.ru>
2017-01-27 17:58:51 +0300

    Add menu list

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #-*- coding: utf-8 -*-
 
+def mainmenu(conn,sock):
+	tmp = ''
+	MenuList = ['1. Выделить ip\r\n','2. Найти пользователя\r\n','3. Удалить пользователя\r\n', '4. Удалить ПК\r\n','5. Выход\r\n']
+	for i in MenuList:
+		conn.send(i.encode('cp866'))
+
+	while "\n" not in tmp:
+		data = conn.recv(1024)
+		tmp = data.decode('cp866')
+		if tmp=="5":
+			break
+			conn.close()
+			
+	print(UnPass)
+
 import socket
 
 sock = socket.socket()
@@ -9,29 +24,21 @@ conn, addr = sock.accept()
 print ('connected:',addr)
 
 a = ''
-s = "Un: "
-conn.send(s.encode('utf-8'))
-UnPass = []
-
-while "\n" not in a:
-	data = conn.recv(1024)
-	a = a + data.decode('utf-8')
-	if "\n" in a:
-		UnPass.append(a.rstrip())
-
-a = ''
 s = "Pw: "
 conn.send(s.encode('utf-8'))
+UnPass=[]
+
 
 while "\n" not in a:
 	data = conn.recv(1024)
-	a = a + data.decode('utf-8')
+	a = a + data.decode('cp866')
 	if "\n" in a:
 		UnPass.append(a.rstrip())
 
-if UnPass[0]=="admin" and UnPass[1]=="Qq123456":
+if UnPass[0]=="админ":
 	print("All ok!!!")
 	s = "All ok!!! "
+	mainmenu(conn,sock)
 	print(UnPass)
 else:
 	conn.close()
